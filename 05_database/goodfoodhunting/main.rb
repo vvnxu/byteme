9c37830cdb8cfcaf856ec13176f6f1376d2f811b
Daniel Tsui <tsui.daniel@gmail.com>
2018-01-12 17:42:22 +1100

    crud app without update

--- 05_database/goodfoodhunting/main.rb
+++ 05_database/goodfoodhunting/main.rb
@@ -1,17 +1,13 @@
 require 'sinatra'
 require 'sinatra/reloader'
-require 'pry'
 require 'pg'
 
 get '/' do
   conn = PG.connect(dbname: 'goodfoodhunting')
-  sql = "SELECT * FROM dishes;"
-  @dishes = conn.exec(sql)
-  binding.pry
+  @dishes = conn.exec("SELECT * FROM dishes;")
   conn.close
   erb :index
 end
-
 
 get '/dishes/new' do
   erb :new
@@ -19,10 +15,11 @@ end
 
 get '/dishes/:id' do
   conn = PG.connect(dbname: 'goodfoodhunting')
-  sql = "SELECT * FROM dishes WHERE ID = #{params[:id]};"
-  @dish = conn.exec(sql).first # exec returns an array of hashes even if it contains one record
+  @dish = conn.exec("SELECT * FROM dishes WHERE id = #{params[:id]}").first
+  conn.close
   erb :show
 end
+
 
 post '/dishes' do
   # this is where we create the record in the db
@@ -30,23 +27,20 @@ post '/dishes' do
   conn = PG.connect(dbname: 'goodfoodhunting')
   # execute a sql statement
   sql = "INSERT INTO dishes (name,image_url) VALUES ('#{params[:name]}','#{params[:image_url]}');"
-  conn.exec(sql)
+
+  result = conn.exec(sql)
   # close the db connection
   conn.close
+
   redirect '/'
 end
 
 post '/dishes/:id/delete' do
-  # return "deleting dish #{params[:id]}... "
-
-  # connect to db
-  # prepare sql with right id
-  # execute
-  # redirect to '/'
+  conn = PG.connect(dbname: 'goodfoodhunting')
+  sql = "DELETE FROM dishes WHERE id = #{params[:id]});"
+  conn.close
+  redirect '/'
 end
-
-
-
 
 
 
