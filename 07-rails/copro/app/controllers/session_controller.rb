ab48dcff6814728ac8eb5b4bfec4c81851a4b720
Daniel Tsui <tsui.daniel@gmail.com>
2018-01-24 09:58:49 +1100

    add password login

---
+++ 07-rails/copro/app/controllers/session_controller.rb
@@ -0,0 +1,18 @@
+class SessionController < ApplicationController
+
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:email])
+
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to '/'
+    else
+      render :new
+    end
+
+  end
+
+end
